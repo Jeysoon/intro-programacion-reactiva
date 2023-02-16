829827283e33be0ab13e4a366211089a4c2d101b
Jeyson <jeysoon@gmail.com>
2023-02-15 18:03:32 -0700

    Create footer and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ComponentsModule } from 'src/components/components/components.module';
+import { FooterComponent } from 'src/components/footer/footer.component';
+import { HeaderComponent } from 'src/components/header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
